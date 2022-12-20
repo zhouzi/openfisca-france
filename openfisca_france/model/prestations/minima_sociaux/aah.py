e376353de652d1ade01f1425bbaaa769a7bc2173
Emmanuel Raviart <emmanuel@raviart.com>
2022-12-20 15:49:48 +0100

    Use Period() instead of .period method

--- openfisca_france/model/prestations/minima_sociaux/aah.py
+++ openfisca_france/model/prestations/minima_sociaux/aah.py
@@ -1,3 +1,5 @@
+from openfisca_core.periods import Period
+
 from openfisca_france.model.base import *
 
 from numpy import datetime64
@@ -57,7 +59,7 @@ class aah_base_ressources(Variable):
             tranche2 = revenus_demandeur - tranche1
             revenus_abattus_smic = (1 - aah.travail_ordinaire.abattement_30) * tranche1 + (1 - aah.travail_ordinaire.abattement_sup) * tranche2
             
-            last_month = period.start.period('month').offset(-1)
+            last_month = Period('month', period.start).offset(-1)
             has_ressources_substitution = (
                 individu('chomage_net', last_month)
                 + individu('retraite_nette', last_month)
@@ -68,7 +70,7 @@ class aah_base_ressources(Variable):
             return abat_cessation_activite * (revenus_abattus_smic)
             
         def base_ressource_eval_trim():
-            three_previous_months = period.first_month.start.period('month', 3).offset(-3)
+            three_previous_months = Period('month', period.first_month.start, 3).offset(-3)
             base_ressource_activite = individu('aah_base_ressources_activite_eval_trimestrielle', period) - individu('aah_base_ressources_activite_milieu_protege', three_previous_months, options = [ADD])
             base_ressource_hors_activite = individu('aah_base_ressources_hors_activite_eval_trimestrielle', period) + individu('aah_base_ressources_activite_milieu_protege', three_previous_months, options = [ADD])
 
@@ -114,8 +116,8 @@ class aah_base_ressources(Variable):
             tranche2 = revenus_demandeur - tranche1
             revenus_abattus_smic = (1 - aah.travail_ordinaire.abattement_30) * tranche1 + (1 - aah.travail_ordinaire.abattement_sup) * tranche2
             
-            previous_year = period.start.period('year').offset(-1)
-            last_month = period.start.period('month').offset(-1)
+            previous_year = Period('year', period.start).offset(-1)
+            last_month = Period('month', period.start).offset(-1)
             ressource_year = individu('aah_base_ressources_eval_annuelle', previous_year, options=[ADD])
             ressource_last_month = individu('aah_base_ressources_eval_annuelle', last_month)
             ressource_interrompue = (ressource_year > 0) * (ressource_last_month == 0)
@@ -130,7 +132,7 @@ class aah_base_ressources(Variable):
             return abat_cessation_activite * (revenus_abattus_smic)
 
         def base_ressource_eval_trim():
-            three_previous_months = period.first_month.start.period('month', 3).offset(-3)
+            three_previous_months = Period('month', period.first_month.start, 3).offset(-3)
             base_ressource_activite = individu('aah_base_ressources_activite_eval_trimestrielle', period) - individu('aah_base_ressources_activite_milieu_protege', three_previous_months, options = [ADD])
             base_ressource_hors_activite = individu('aah_base_ressources_hors_activite_eval_trimestrielle', period) + individu('aah_base_ressources_activite_milieu_protege', three_previous_months, options = [ADD])
 
@@ -181,7 +183,7 @@ class aah_base_ressources_activite_eval_trimestrielle(Variable):
 
     def formula(individu, period):
         period = period.first_month
-        three_previous_months = period.start.period('month', 3).offset(-3)
+        three_previous_months = Period('month', period.start, 3).offset(-3)
         last_year = period.last_year
 
         ressources_a_inclure = [
@@ -241,7 +243,7 @@ class aah_base_ressources_hors_activite_eval_trimestrielle(Variable):
 
     def formula(individu, period):
         period = period.first_month
-        three_previous_months = period.start.period('month', 3).offset(-3)
+        three_previous_months = Period('month', period.start, 3).offset(-3)
 
         ressources_a_inclure = [
             'asi',
@@ -439,7 +441,7 @@ class eligibilite_caah(Variable):
     set_input = set_input_dispatch_by_period
 
     def formula_2015_07_01(individu, period, parameters):
-        annee_precedente = period.start.period('year').offset(-1)
+        annee_precedente = Period('year', period.start).offset(-1)
         prestations = parameters(period).prestations_sociales
         taux_incapacite_min = prestations.prestations_etat_de_sante.invalidite.aah.taux_capacite.taux_incapacite
         aah = individu('aah', period)
