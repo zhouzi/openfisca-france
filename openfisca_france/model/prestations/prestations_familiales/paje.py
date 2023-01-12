89133bf56bc4d07db360191c1a1e1144ddeeee5e
Sasha-Laniece <sasha.laniece@gmail.com>
2023-01-12 16:10:09 +0100

    Fix description

--- openfisca_france/model/prestations/prestations_familiales/paje.py
+++ openfisca_france/model/prestations/prestations_familiales/paje.py
@@ -439,8 +439,6 @@ class paje_cmg(Variable):
         etudiant_i = famille.members('etudiant', period)
         parent_etudiant = famille.any(etudiant_i, role=Famille.PARENT)
 
-        # condition de revenu minimal
-
         # L'enfant doit avoir un age (0-6 ans) ou (0-12 ans si parents isolé)
         cond_age_enf = (
             (nb_enf(famille, period, 0, paje.paje_cmg.limite_age.pleine - 1) > 0) | (parent_isole * (nb_enf(famille, period, 0, paje.paje_cmg.limite_age.etendue - 1) > 0)))
@@ -451,9 +449,8 @@ class paje_cmg(Variable):
 
         cond_eligibilite = cond_age_enf & (not_(inactif) | cond_non_act)
 
-        # Si vous bénéficiez de la PreParE taux plein
-        # (= vous ne travaillez plus ou interrompez votre activité professionnelle),
-        # vous ne pouvez pas bénéficier du Cmg.
+        # TODO: voir les montants partiels si PreParE
+        # Si vous bénéficiez de la PreParE taux plein (= vous ne travaillez plus ou interrompez votre activité professionnelle), vous ne pouvez pas bénéficier du Cmg.
         paje_prepare_inactif = (paje_prepare > 0) * inactif
         eligible = cond_eligibilite * not_(paje_prepare_inactif)
 
