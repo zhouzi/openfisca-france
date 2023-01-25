a9d9f31c4c19336ffcf6d311be01fa2ce995f526
Brice Fabre <b.fabre@pse.local>
2023-01-25 16:25:23 +0100

    Supprime variable bourse_enseignement_sup

--- openfisca_france/model/prestations/aides_exceptionnelles/prime_exceptionnelle_rentree.py
+++ openfisca_france/model/prestations/aides_exceptionnelles/prime_exceptionnelle_rentree.py
@@ -47,7 +47,7 @@ class eligibilite_per_etudiant(Variable):
     def formula_2022(individu, period):
         juin_2022 = periods.period('2022-06')
         eligibilite_etudiant = where(
-            not_(or_(individu('bourse_criteres_sociaux', juin_2022) > 0, individu('bourse_enseignement_sup', juin_2022) > 0)),
+            not_(individu('bourse_criteres_sociaux', juin_2022) > 0),
             False,
             where(
                 and_(individu.has_role(Famille.PARENT), individu.famille('eligibilite_per', period)),
