2cf079002690d168981feade77c90ee59adf7b41
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-09-06 12:39:28 +0200

    Ajoute le montant de l'indemnité service civique

--- openfisca_france/model/prestations/jeunes/service_civique.py
+++ openfisca_france/model/prestations/jeunes/service_civique.py
@@ -1,4 +1,4 @@
-from openfisca_france.model.base import Variable, Individu, MONTH, set_input_dispatch_by_period
+from openfisca_france.model.base import Variable, Individu, MONTH, set_input_dispatch_by_period, set_input_divide_by_period
 
 
 class service_civique(Variable):
@@ -8,3 +8,16 @@ class service_civique(Variable):
     definition_period = MONTH
     set_input = set_input_dispatch_by_period
     reference = 'https://www.service-public.fr/particuliers/vosdroits/F13278'
+
+
+class montant_indemnite_service_civique(Variable):
+    value_type = float
+    label = "Montant de l'indemnité de Service Civique"
+    entity = Individu
+    definition_period = MONTH
+    set_input = set_input_divide_by_period
+
+    def formula_2022_07_01(individu, period, parameters):
+        service_en_cours = individu('service_civique', period)
+        montant = parameters(period).prestations_sociales.education.service_civique.montants_indemnites
+        return montant * service_en_cours
